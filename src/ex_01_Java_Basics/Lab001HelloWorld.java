822e430dc57f61b34e9732ef35c2ac5fc0bf4c14
PrasannaSai21 <Yedururikumar@gmail.com>
2025-05-06 22:26:31 +0530

    first commit

--- src/ex_01_Java_Basics/Lab001HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001HelloWorld.java
@@ -3,6 +3,6 @@ package ex_01_Java_Basics;
 public class Lab001HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println();
+        System.out.println("hello");
     }
 }
