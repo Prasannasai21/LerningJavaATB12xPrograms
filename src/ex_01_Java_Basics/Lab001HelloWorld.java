ee64986dbe77a1f0e9e35ee78c25aa1412d388f5
PrasannaSai21 <Yedururikumar@gmail.com>
2025-05-06 23:01:27 +0530

    Merge remote-tracking branch 'origin/main'

--- src/ex_01_Java_Basics/Lab001HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001HelloWorld.java
@@ -3,7 +3,7 @@ package ex_01_Java_Basics;
 public class Lab001HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        System.out.println("hello 12455");
         System.out.println("HI");
     }
 }
